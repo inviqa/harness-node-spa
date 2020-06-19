50fe33d515acc9ad13b0e0f53505c1ccfe48a803
Andy Thompson <athompson@inviqa.com>
2020-06-19 17:35:54 +0100

    Fix typo in nginx entrypoint script

--- docker/image/nginx/root/docker-entrypoint.sh
+++ docker/image/nginx/root/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 CONFIG="$(echo '{}' | jq -f -c /root/config.jq -)"
 ESCAPED_CONFIG=$(printf '%s\n' "$CONFIG" | sed -e 's/[\/&]/\\&/g')
 
-sed -i 's/%CONFIG%/'"$ESCAPED_CONFIG"'/' sr/share/nginx/html/index.html
+sed -i 's/%CONFIG%/'"$ESCAPED_CONFIG"'/' /usr/share/nginx/html/index.html
 
 exec "$@"
