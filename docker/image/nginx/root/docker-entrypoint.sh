3bf4de4e50745c57d1367e99e46eb1f0007ea155
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2020-08-17 18:28:55 +0100

    Escape \ and & still due to sed escaping (#18)

--- docker/image/nginx/root/docker-entrypoint.sh
+++ docker/image/nginx/root/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONFIG="$(echo '{}' | jq -f -c /root/config.jq -)"
-ESCAPED_CONFIG=$(printf '%s\n' "$CONFIG" | sed -e 's/&/\&amp;/g; s/"/\&quot;/g')
+ESCAPED_CONFIG=$(printf '%s\n' "$CONFIG" | sed -e 's/&/\&amp;/g; s/"/\&quot;/g; s/[\/&]/\\&/g')
 
 sed -i 's/%CONFIG%/'"$ESCAPED_CONFIG"'/' /usr/share/nginx/html/index.html
 
