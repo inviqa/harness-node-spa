31bd60207d5aa0500002d4fa671f281aab6c7aab
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2020-08-17 18:04:10 +0100

    Escape & and " in config expected to be in html attribute (#17)

--- docker/image/nginx/root/docker-entrypoint.sh
+++ docker/image/nginx/root/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONFIG="$(echo '{}' | jq -f -c /root/config.jq -)"
-ESCAPED_CONFIG=$(printf '%s\n' "$CONFIG" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_CONFIG=$(printf '%s\n' "$CONFIG" | sed -e 's/&/\&amp;/g; s/"/\&quot;/g')
 
 sed -i 's/%CONFIG%/'"$ESCAPED_CONFIG"'/' /usr/share/nginx/html/index.html
 
